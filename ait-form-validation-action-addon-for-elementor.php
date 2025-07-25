64ce4231a623f7707e43d9f0599e7c3f396d0bd0
Andrés Ignacio Torres <dev@aitorres.com>
2025-07-24 21:54:59 -0700

    version update

--- ait-form-validation-action-addon-for-elementor.php
+++ ait-form-validation-action-addon-for-elementor.php
@@ -39,7 +39,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'AIT_VALIDATION_ACTION_ELEMENTOR_FORMS_VERSION', '1.3.0' );
+define( 'AIT_VALIDATION_ACTION_ELEMENTOR_FORMS_VERSION', '1.4.0' );
 
 /**
  * The core plugin class that is used to define internationalization,
